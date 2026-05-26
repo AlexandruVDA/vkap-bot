8c2dc57419bf9a26bcdae8b3420616fd7a627ccc
AlexandruVDA <80089108+AlexandruVDA@users.noreply.github.com>
2026-05-26 16:22:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const MAX_TRADE_SOL = 0.5;
 
 let autoBuyEnabled = true;
 
-const AUTO_BUY_AMOUNT = 0.0002;
+const AUTO_BUY_AMOUNT = 0.0001;
 const AUTO_BUY_INTERVAL = 15 * 60 * 1000;
 const FEE_BUFFER_SOL = 0.01;
 
